87e04d8a3e077b415a41524bff62861b03e54670
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-10-16 01:53:20 +0200

    Make it possible to play videoes from browser

--- server.js
+++ server.js
@@ -1,18 +1,57 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
+
+const app = express()
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
 
 let videoes = []
 try {
   videoes = require('./videoes/videoes')
 } catch (ex) {}
 
-async function playVideo(video) {
+async function playVideo(video, ws = null) {
   const { stdout } = await exec(
-    `ffmpeg -i ${video} 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,//`,
+    `ffmpeg -i ./videoes/${video} 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,//`,
   )
   const [hours, minutes, seconds] = stdout.split(':').map(e => parseFloat(e))
   const time = hours * 3600 + minutes * 60 + seconds
-  exec(`timeout ${time}s vlc ${video}`)
+  try {
+    await exec(`timeout ${time}s vlc ./videoes/${video} --fullscreen`)
+  } catch (e) {}
+
+  sendMessage(ws, 'done', {
+    file: video,
+  })
 }
 
-//playVideo('./videoes/video1.webm')
+function sendMessage(ws, status, data = {}) {
+  if (ws) {
+    ws.send(
+      JSON.stringify({
+        status,
+        ...data,
+      }),
+    )
+  }
+}
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    playVideo(message, ws)
+    sendMessage(ws, 'play', {
+      file: message,
+    })
+  })
+
+  sendMessage(ws, 'init', {
+    videoes,
+  })
+})
+
+server.listen(process.env.PORT || 8999, () => {
+  console.log(`Running on http://localhost:${server.address().port}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [videoes, setVideoes] = useState([])
+  const [ws, setWs] = useState(null)
+
+  useEffect(() => {
+    var ws = new WebSocket('ws://localhost:8999')
+    setWs(ws)
+    ws.onopen = function() {}
+    return () => {
+      setWs(null)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (ws) {
+      ws.onmessage = ({ data }) => {
+        try {
+          const { status, ...result } = JSON.parse(data)
+          switch (status) {
+            case 'init':
+              setVideoes(result.videoes)
+              break
+            case 'play':
+              const videoesCpy1 = videoes.map(e => ({
+                ...e,
+                playing: e.file === result.file,
+              }))
+              setVideoes(videoesCpy1)
+              break
+            case 'done':
+              const videoesCpy2 = videoes.map(e => ({
+                ...e,
+                playing: e.file === result.file ? false : !!e.playing,
+              }))
+              setVideoes(videoesCpy2)
+              break
+            default:
+              break
+          }
+        } catch (e) {}
+      }
+    }
+  }, [ws, videoes])
+
+  const playVideo = video => {
+    if (ws) ws.send(video)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {videoes.map((e, i) => (
+          <button key={i} onClick={() => playVideo(e.file)} title={e.desc}>
+            Play{e.playing ? 'ing' : ''} {e.name}
+          </button>
+        ))}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
